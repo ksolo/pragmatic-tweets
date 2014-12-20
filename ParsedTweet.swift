173cb87018bb515bbaa1a123f29de94d1ddc9dc8
Kevin Solorio <kevin@detone8.com>
2014-12-19 20:19:00 -0500

    added ParsedTweet class

---
+++ ParsedTweet.swift
@@ -0,0 +1,31 @@
+//
+//  ParsedTweet.swift
+//  PragmaticTweets
+//
+//  Created by Kevin Solorio on 12/19/14.
+//  Copyright (c) 2014 Kevin Solorio. All rights reserved.
+//
+
+import UIKit
+
+class ParsedTweet: NSObject {
+ 
+    var tweetText : String?
+    var userName : String?
+    var createdAt : String?
+    var userAvatarURL : NSURL?
+    
+    init(tweetText: String?, userName: String?, createdAt: String?, userAvatarURL: NSURL?){
+        super.init()
+        
+        self.tweetText = tweetText
+        self.userName = userName
+        self.createdAt = createdAt
+        self.userAvatarURL = userAvatarURL
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+}
